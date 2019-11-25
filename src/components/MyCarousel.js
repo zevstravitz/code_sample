979dda12e05c66f78d27ceaab72f17898f35d479
Zev Stravitz <zevstravitz2021@u.northwestern.edu>
2019-11-25 23:58:14 +0000

    almost done with first two problems

--- src/components/MyCarousel.js
+++ src/components/MyCarousel.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+
+import Button from 'react-bootstrap/Button';
 import Carousel from 'react-bootstrap/Carousel';
 
 import '../styling/MyCarousel.css';
@@ -16,31 +18,31 @@ class MyCarousel extends Component {
         title: 'The Fourth Phase',
         text: 'From the creators of The Art of Flight, Red Bull Media House presents THE FOURTH PHASE, a nowboarding epic starring iconic athlete Travis Rice.',
         image: fourthphase,
-        color: "blue"
+        color: "#3e6aa7"
       },
       {
         title: 'Hunt for the WildPeople',
         text: 'Raised on hip-hop and foster care, defiant city kid Ricky gets a fresh start in the New Zealand countryside. From the director of What We Do In The Shadows.',
         image: hunt,
-        color: "blue"
+        color: "#3a56b5"
       },
       {
         title: 'Nichts passiert / A Decent Man',
         text: 'A Swiss family takes a ski vacation and runs into trouble when the father, the titular decent man, finds himself in a series of moral quandries.',
         image: nichts,
-        color: "navy"
+        color: "#4bc4f1"
       },
       {
         title: 'Vice Versa',
-        text: '',
+        text: 'Come along with the Good Company crew as they travel thoughout the US, Japan, BC and Quebec to showcase skiiing in the best way possible.',
         image: viceversa,
-        color: "gold"
+        color: "#b1a726"
       },
       {
         title: 'Fullmoon',
-        text: '',
+        text: 'Be inspired by women who push boundaries. FULL MOON showcases the legends, current icons and future prodigies of this ever-evolving lifefstyle sport.',
         image: fullmoon,
-        color: "gold"
+        color: "#979799"
       },
     ],
   }
@@ -49,8 +51,8 @@ class MyCarousel extends Component {
     return (
       <div className="App-Carousel">
         <Carousel className="carousel-body" indicators={ false }>
-              {this.state.movies.map((movies) => {
-                return <Carousel.Item>
+              {this.state.movies.map((movies, index) => {
+                return <Carousel.Item key={index}>
                     <div >
                       <img
                         className="Carousel-image"
@@ -58,10 +60,25 @@ class MyCarousel extends Component {
                         alt= {movies.title}
                       />
                     </div>
-                    <div>
+                    <div className="info">
                     <Carousel.Caption>
-                      <h3>{ movies.title}</h3>
-                      <p>{ movies.text}</p>
+                        <div className="movie-poster">
+                          <img
+                            src={ movies.image }
+                            alt= {movies.title}
+                            height="300px"
+                            className="poster"
+                          />
+                        </div>
+                      <div className="info">
+                        <h3>{ movies.title}</h3>
+                        <p>{ movies.text}</p>
+                        <Button id="buy"
+                                style={{ backgroundColor: movies.color,
+                                         borderColor: movies.color }}
+                                >Buy Now</Button>
+                        <Button id="trailer" variant="outline-secondary">Watch Trailer</Button>
+                      </div>
                     </Carousel.Caption>
                   </div>
                </Carousel.Item>
--- src/components/Panels.js
+++ src/components/Panels.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import App from '../App';
 
 import '../styling/Panels.css';
 
